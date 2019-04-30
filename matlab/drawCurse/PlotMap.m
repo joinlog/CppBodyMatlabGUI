a9f54e37a54a4b5bc295b1acb5ac63fde5871d0a
zhaoyun <zhaoyun@zpmc.com>
2019-04-30 23:08:50 +0800

    更新画图坐标说明

--- matlab/drawCurse/PlotMap.m
+++ matlab/drawCurse/PlotMap.m
@@ -26,8 +26,8 @@ t=1:m;
 plot(t,S,'-s',t,E,'-o',t,I,'-*r',t,R,'-d');
 legend('S','E','I','R');
 %title('Title','FontName','Times New Roman','FontWeight','Bold','FontSize',16)
-xlabel('Time','FontName','Times New Roman','FontSize',14)
-ylabel('Cell Status Percentage','FontName','Times New Roman','FontSize',14,'Rotation',90)
+xlabel('迭代次数','FontName','Times New Roman','FontSize',14)
+ylabel('元胞节点状态比率','FontName','Times New Roman','FontSize',14,'Rotation',90)
 
 
 hold on;
